4e07dd3c788808fe9b8d78a3c9c46bead6d6774a
namitav <Namitav@Proconstructor.com>
2011-11-09 15:06:29 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProrailsApp::Application.config.secret_token = 'cc3290479a1118e9f8a72084979643765500559eadc6ef9f0378fe3e0388aaaef43f3c3ac9ce6d09fd79a3516c37e210dce4f2cfad130e92b52ad3f78972f21e'
